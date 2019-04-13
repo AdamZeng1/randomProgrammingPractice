ef338198382b4cceeba966c07026f71b72706965
adamzeng <khklara3@gmail.com>
2019-04-13 21:48:21 +1000

    retrive data from database

--- php/showProductDetail.php
+++ php/showProductDetail.php
@@ -1,7 +1,44 @@
 <?php
 
 if (isset($_REQUEST['data'])) {
-    echo $_REQUEST['data'];
+
+    // if get the request data from the html, just try to get the data from the database
+
+//    $connection = mysqli_connect('rerun.it.uts.edu.au', 'potiro', 'pcXZb(kL', 'poti');
+    $connection = mysqli_connect('localhost:3306', 'root', 'zengweihan', 'assignment');
+
+    $id = $_REQUEST['data'];
+
+
+    $query_string = "select * from products where product_id = " . $id;
+    $result = mysqli_query($connection, $query_string);
+
+
+    $num_rows = mysqli_num_rows($result);
+    if ($num_rows > 0) {
+        print "<table border='1'> ";
+
+        if ($a_row = mysqli_fetch_array($result)) {
+            print "<br>";
+
+            {
+                echo $a_row["product_name"] . "<br>";
+                echo $a_row["unit_price"] . "<br>";
+                echo $a_row["unit_quantity"] . "<br>";
+                echo $a_row["in_stock"] . "<br>";
+
+            }
+        }
+
+        print "</table>";
+
+
+        // display a button here
+        print "<button type=\"button\" style=\"position:absolute;bottom:8px;right:16px;font-size:18px;background-color:#4CAF50;\">Add</button>";
+    }
+    mysqli_close($connection);
+
+
 } else {
     echo "This is top-right.";
 }
